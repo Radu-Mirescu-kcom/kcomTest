7e37c5e012e984317f7f4dd342c0a7b8ab2fa8e0
Constantin-Radu Mirescu <radu.mirescu@rinftech.com>
2016-10-18 11:09:06 +0300

    prepare for change for optimal solution

--- src/main/java/ro/rinf/kcom/devtest/WithCoinLimitOptimalChangeContext.java
+++ src/main/java/ro/rinf/kcom/devtest/WithCoinLimitOptimalChangeContext.java
@@ -1,11 +1,15 @@
 package ro.rinf.kcom.devtest;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 public class WithCoinLimitOptimalChangeContext extends NoCoinLimitOptimalChangeContext {
     private InventoryIterator inventoryIterator;
     private Optional<Coin> optionalCoin;
+    private List<Collection<Coin>> candidates = new ArrayList<>();
+    private Coin lastCoin;
 
     public WithCoinLimitOptimalChangeContext(Inventory inventory, int amount) {
         super(amount);
@@ -23,23 +27,38 @@ public class WithCoinLimitOptimalChangeContext extends NoCoinLimitOptimalChangeC
         inventoryIterator.takeOne();
     }
 
+    private void tryOtherPath() {
+        lastCoin = toReturn.remove(toReturn.size()-1);
+        amount += lastCoin.getDenomination();
+        optionalCoin = inventoryIterator.resetToNext(lastCoin);
+    }
+
     @Override
     public Collection<Coin> getChangeFor() {
+        boolean working = true;
         getNextCoin();
-        while(needsCoin()) {
-            if( optionalCoin.get().fitsIn(amount) ) {
-                addCoin(optionalCoin.get());
-            } else {
-                getNextCoin();
-                while( !optionalCoin.isPresent() ) {
-                    if( toReturn.isEmpty() ) {
-                        throw new InsufficientCoinageException();
+        while(working) {
+            while(working && needsCoin()) {
+                if (optionalCoin.get().fitsIn(amount)) {
+                    addCoin(optionalCoin.get());
+                } else {
+                    getNextCoin();
+                    while (!optionalCoin.isPresent()) {
+                        if (toReturn.isEmpty()) {
+                            if (candidates.isEmpty()) {
+                                throw new InsufficientCoinageException();
+                            } else {
+                                working = false;
+                                break;
+                            }
+                        } else {
+                            tryOtherPath();
+                        }
                     }
-                    Coin lastCoin = toReturn.remove(toReturn.size()-1);
-                    amount += lastCoin.getDenomination();
-                    optionalCoin = inventoryIterator.resetToNext(lastCoin);
                 }
             }
+            candidates.add(toReturn);
+            working = false;
         }
         return toReturn;
     }
